a2a8d1eb88cff458be39c9e99d37a4e46547636a
JC <subnuggurat@gmail.com>
2024-02-29 03:58:24 -0600

    bassic java functionality set up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,33 @@
- document.getElementById('test-div').innerHTML = 'It works!';
+document.getElementById("test-div").innerHTML = "It works!";
 
+function hideAllItems() {
+  //get all items
+  var allItems = document.querySelectorAll(".list-group-item");
+  //set display to none
+  allItems.forEach(function (allItem) {
+    allItem.classList.add("d-none");
+  });
+}
 
+hideAllItems();
 
-document.getElementById('options-list').addEventListener('click', function(e){
-    e.preventDefault();
+document.getElementById("options-list").addEventListener("click", function (e) {
+  e.preventDefault();
 
-    //Get link's id
-    if(e.target.tagName === 'A'){
-        var myChoice = (e.target.id)
-    };
+  //Get link's id
+  if (e.target.tagName === "A") {
+    var myChoice = e.target.id;
+  }
+  hideAllItems();
+  //get list of items
+  var chosenItems = document.querySelectorAll(
+    '[data-month = "' + myChoice + '"]'
+  );
 
+  //set item visibility
+  chosenItems.forEach(function (chosenItem) {
+    chosenItem.classList.remove("d-none");
+  });
 
-    console.log(myChoice);
+  console.log(myChoice, chosenItems);
 });
