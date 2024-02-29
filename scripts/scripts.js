380b6acf469894766cc79ee81de76826d1874f26
JC <subnuggurat@gmail.com>
2024-02-29 02:07:01 -0600

    JAva baby steps 	modified:   index.html 	modified:   scripts/scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,15 @@
  document.getElementById('test-div').innerHTML = 'It works!';
- 
+
+
+
+document.getElementById('options-list').addEventListener('click', function(e){
+    e.preventDefault();
+
+    //Get link's id
+    if(e.target.tagName === 'A'){
+        var myChoice = (e.target.id)
+    };
+
+
+    console.log(myChoice);
+});
