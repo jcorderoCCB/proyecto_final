667360e470b79bf141b41ceacef585d3caa193e6
JC <subnuggurat@gmail.com>
2024-02-29 21:25:55 -0600

    Java functionality done! Dropdown styling and dynamic name Added bottom div for padding on mobile

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,3 @@
-document.getElementById("test-div").innerHTML = "It works!";
-
 function hideAllItems() {
   //get all items
   var allItems = document.querySelectorAll(".list-group-item");
@@ -8,16 +6,22 @@ function hideAllItems() {
     allItem.classList.add("d-none");
   });
 }
-
+//hides all items on load
 hideAllItems();
 
+//listens to changes on dropdown
 document.getElementById("options-list").addEventListener("click", function (e) {
-  e.preventDefault();
+  //e.preventDefault();
 
   //Get link's id
   if (e.target.tagName === "A") {
     var myChoice = e.target.id;
   }
+//Get link's text
+  var monthName = document.getElementById(myChoice).textContent;
+//set month name text
+  document.getElementById("myDropdown").textContent = monthName;
+  //reset item visibility to hidden
   hideAllItems();
   //get list of items
   var chosenItems = document.querySelectorAll(
@@ -29,5 +33,5 @@ document.getElementById("options-list").addEventListener("click", function (e) {
     chosenItem.classList.remove("d-none");
   });
 
-  console.log(myChoice, chosenItems);
+  console.log(myChoice, chosenItems, monthName);
 });
